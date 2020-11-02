7c78cb585bc77016a00383bef24c160e26ece0f8
AlexeyAnufriev <contact@alexey-anufriev.com>
2020-11-03 00:35:44 +0100

    cleanup: remove redundant class name normalization

--- mapping-dsl-generator/mapping-dsl-generator-core/src/main/java/io/mappingdsl/generator/core/utils/GeneratorUtils.java
+++ mapping-dsl-generator/mapping-dsl-generator-core/src/main/java/io/mappingdsl/generator/core/utils/GeneratorUtils.java
@@ -10,13 +10,7 @@ public final class GeneratorUtils {
     private static final String DSL_CLASS_NAME_SUFFIX = "MappingDsl";
 
     public static String getDslWrapperClassName(String fullClassName) {
-        String normalizedClassName = normalizeClassName(fullClassName);
-
-        if (normalizedClassName.indexOf('.') >= 0) {
-            return normalizedClassName.substring(normalizedClassName.lastIndexOf('.') + 1) + DSL_CLASS_NAME_SUFFIX;
-        }
-
-        return fullClassName + DSL_CLASS_NAME_SUFFIX;
+        return getClassName(fullClassName) + DSL_CLASS_NAME_SUFFIX;
     }
 
     public static boolean isDslWrapperClass(String fullClassName) {
@@ -25,28 +19,19 @@ public final class GeneratorUtils {
 
     @Nullable
     public static String getClassPackage(String fullClassName) {
-        String normalizedClassName = normalizeClassName(fullClassName);
-
-        if (normalizedClassName.indexOf('.') >= 0) {
-            return normalizedClassName.substring(0, normalizedClassName.lastIndexOf('.'));
+        if (fullClassName.indexOf('.') >= 0) {
+            return fullClassName.substring(0, fullClassName.lastIndexOf('.'));
         }
 
         return null;
     }
 
     public static String getClassName(String fullClassName) {
-        String normalizedClassName = normalizeClassName(fullClassName);
-
-        if (normalizedClassName.indexOf('.') >= 0) {
-            return normalizedClassName.substring(normalizedClassName.lastIndexOf('.') + 1);
+        if (fullClassName.indexOf('.') >= 0) {
+            return fullClassName.substring(fullClassName.lastIndexOf('.') + 1);
         }
 
         return fullClassName;
-    }
-
-    private static String normalizeClassName(String className) {
-        // align filesystem path with regular package naming
-        return className.replace('/', '.');
     }
 
 }
